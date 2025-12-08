dbecc42424429caddb7b97940a6a85f35e2b7bb1
unknown <musou6413@naver.com>
2025-12-08 16:49:55 +0900

    feat: 급식 정보 페이지 (NEIS API + Local 캐싱 + 필터링)
    - Dio, Sqflite 의존성 추가
    - provider.dart 의존성 관리
    - NEIS API 급식정보 요청 로직
    - Sqflite 로컬 DB / 캐싱 로직 / 만료 규칙
    - 필터링 및 파싱 로직
    - 급식 정보 페이지 위젯들

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:damta/core/config/routes.dart';
+import 'package:damta/data/database/database_helper.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(ProviderScope(child: const MyApp()));
 }
 
@@ -10,9 +13,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp.router(routerConfig: router, debugShowCheckedModeBanner: false);
   }
 }
