390091a8ef25c70a17f8d83a81d7b1001f98f95a
hailey <ahhh0325@gmail.com>
2025-12-08 10:06:06 +0900

    design: 알림 페이지, 알림 아이콘 UI 구현

---
+++ lib/presentation/view/pages/home/widgets/noti_button.dart
@@ -0,0 +1,35 @@
+import 'package:damta/core/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NotiButton extends StatelessWidget {
+  const NotiButton({super.key});
+
+  // 새로운 알림 여부
+  // TODO : 더미데이터 지우기
+  final bool newNoti = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => context.go('/notification'),
+      child: SizedBox(
+        width: 60,
+        height: 60,
+        child: Stack(
+          children: [
+            Center(
+              child: Icon(Icons.notifications_none, size: 30, color: darkgrey),
+            ),
+            newNoti
+                ? Align(
+                    alignment: Alignment(0.3, -0.3),
+                    child: Icon(Icons.circle, size: 10, color: Colors.red),
+                  )
+                : SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
