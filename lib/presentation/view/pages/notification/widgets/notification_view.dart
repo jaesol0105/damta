8fd1d53bf9ed03346441710f15f1deaabcc08290
Hailey-crypto <ahhh0325@gmail.com>
2025-12-08 20:40:12 +0900

    design: 알림 페이지, 시간표 페이지 UI 구현, 테마 - hr (#5)
    - design: 시간표 페이지 UI 구현
    - design: 알림 페이지, 알림 아이콘 UI 구현
    - design: 날씨 정보 제공 UI 구현
    - feat: 라이트/다크 테마 적용

---
+++ lib/presentation/view/pages/notification/widgets/notification_view.dart
@@ -0,0 +1,58 @@
+import 'package:damta/core/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NotificationView extends StatelessWidget {
+  const NotificationView({
+    super.key,
+    required this.id,
+    required this.isRead,
+    required this.title,
+    this.content,
+    required this.time,
+  });
+
+  final String id;
+  final bool isRead;
+  final String title;
+  final String? content;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        context.go('/post/:$id');
+        // TODO : isRead = true 변경하기
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: isRead ? Colors.transparent : blue,
+          border: Border(bottom: BorderSide(color: grey)),
+        ),
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 7,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // spacing: 15,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                // isRead
+                //     ? SizedBox.shrink()
+                //     : Icon(Icons.circle, color: Colors.red, size: 8),
+              ],
+            ),
+            content == null ? SizedBox.shrink() : Text(content!),
+            Text(time),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/view/pages/time_table/time_table_page.dart
@@ -0,0 +1,85 @@
+import 'package:damta/core/app_theme.dart';
+import 'package:damta/presentation/view/pages/time_table/widgets/choose_class.dart';
+import 'package:damta/presentation/view/pages/time_table/widgets/choose_semester.dart';
+
+import 'package:damta/presentation/view/pages/time_table/widgets/time_table.dart';
+import 'package:flutter/material.dart';
+
+// Entity 로 빼기
+class TableEntity {
+  const TableEntity({
+    required this.year,
+    required this.semester,
+    required this.grade,
+    required this.classes,
+    required this.weekday,
+    required this.period,
+    required this.subject,
+  });
+
+  final int year; // 학년도
+  final int semester; // 학기
+  final int grade; // 학년
+  final int classes; // 반
+  final int weekday; // 요일 (1=월, 2=화, 3=수, 4=목, 5=금)
+  final int period; // 교시
+  final String subject; // 과목
+}
+
+class TimeTablePage extends StatelessWidget {
+  const TimeTablePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back, color: darkgrey),
+        title: SizedBox(
+          width: double.infinity,
+          child: Text('도장중학교', style: TextStyle(fontSize: 18)),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: darkgrey),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  constraints: BoxConstraints(minHeight: 110),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "시간표",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Spacer(),
+                          ChooseSemester(),
+                        ],
+                      ),
+                      Row(children: [Spacer(), ChooseClass()]),
+                    ],
+                  ),
+                ),
+                Divider(color: darkgrey),
+                TimeTable(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
